b8bb61697201d213282fda61e0217c6ad36dd9be
敬洪波 <jinghongbo@live.com>
2018-07-06 21:21:15 +0800

    admin-lte init

---
+++ npm/packs/admin-lte/abp.resourcemapping.js
@@ -0,0 +1,6 @@
+﻿module.exports = {
+    mappings: {
+        "@node_modules/admin-lite/dist/js/adminlte.min.js": "@libs/admin-lte/js",
+        "@node_modules/admin-lite/dist/css/adminlte.min.css": "@libs/admin-lte/css"
+    }
+}
